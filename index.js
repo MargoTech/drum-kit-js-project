c50b4b68d6d89b19919cce33c31fc05e1774946d
MargoTech <margobartashevich@gmail.com>
2025-01-29 14:48:27 +0100

    adding SetTimeout

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ for (var i = 0; i < numberOfDrumBtn; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHtml = this.innerHTML;
     makeSound(buttonInnerHtml);
+    buttonAnimation(buttonInnerHtml);
   });
 }
 
 document.addEventListener("keydown", function () {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -44,4 +46,14 @@ function makeSound(key) {
     default:
       console.log(buttonInnerHtml);
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
